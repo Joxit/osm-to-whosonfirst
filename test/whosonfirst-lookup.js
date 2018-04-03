894dcb57b311a32823bd50db3576e762de3abb28
Joxit <joxit972@gmail.com>
2018-04-03 23:18:53 +0200

    Use pelias-wof-admin-lookup for local lookup and request for remote

--- test/whosonfirst-lookup.js
+++ test/whosonfirst-lookup.js
@@ -1,29 +1,26 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const fs = require('fs');
 const WhosonfirstLookup = require('../lib/whosonfirst-lookup');
-const villetaneuseLocality = JSON.parse(fs.readFileSync('./test/resources/whosonfirst-data/data/101/751/173/101751173.geojson'));
+
 describe('Who\'s On First lookup', () => {
   it('should load data from directory', done => {
-    let wofLookup = new WhosonfirstLookup({directory: './test/resources/whosonfirst-data'});
+    let wofLookup = new WhosonfirstLookup({ directory: './test/resources/whosonfirst-data' });
     wofLookup.load().then(() => {
-      expect(wofLookup.features).to.not.be.undefined;
-      expect(wofLookup.features['locality']).to.not.be.undefined;
-      expect(wofLookup.features['locality']).to.be.lengthOf(1);
-      expect(wofLookup.features['localadmin']).to.be.lengthOf(1);
-      expect(wofLookup.features['empire']).to.be.undefined;
+      expect(wofLookup.pipService).to.not.be.undefined;
       done();
     });
   });
   it('should return polygon', (done) => {
-    let wofLookup = new WhosonfirstLookup({directory: './test/resources/whosonfirst-data'});
+    let wofLookup = new WhosonfirstLookup({ directory: './test/resources/whosonfirst-data' });
     wofLookup.load().then(() => {
-      expect(wofLookup.lookup({
+      return wofLookup.lookup({
         lat: 48.957372,
         long: 2.345141,
-        layer: 'locality'
-      })).to.be.deep.equal(villetaneuseLocality)
+        layers: ['locality']
+      });
+    }).then(r => {
+      expect(r[0].Id).to.be.equal(101751173)
       done();
     });
-  })
+  });
 });
