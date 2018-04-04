78cd90147fba6fc9a62179b7b1d45e789d177513
Joxit <joxit972@gmail.com>
2018-04-04 13:21:05 +0200

    Add example for admin express dataset from http://professionnels.ign.fr/adminexpress

---
+++ examples/admin-express.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const http = Promise.promisifyAll(require('http'));
+const request = Promise.promisifyAll(require('request'));
+const ReadShapefile = require('../lib/read-shapefile');
+const ReprojectGeoJSON = require('../lib/reproject-geojson');
+const whosonfirst = require('pelias-whosonfirst');
+const sink = require('through2-sink');
+const pointOnFeature = require('@turf/point-on-feature');
+const WofLookup = require('../lib/whosonfirst-lookup');
+const features = [];
+const reproject = new ReprojectGeoJSON(2154, 4326);
+const shapefile = process.argv[2];
+const dbf = process.argv[3];
+const pipServiceUrl = process.argv[4];
+if (!pipServiceUrl) {
+  console.error(`Arg missing. Usage : ${process.argv[1]} shapefile dbf pip-service-url`);
+  process.exit(1);
+}
+const wofLookup = new WofLookup({url: pipServiceUrl, layers: ['locality', 'localadmin']})
+const localities = {
+"type": "FeatureCollection",
+"name": "localities",
+"features": []
+};
+const localadmin = {
+"type": "FeatureCollection",
+"name": "localadmin",
+"features": []
+};
+const localityAndLocaladmin = {
+"type": "FeatureCollection",
+"name": "newlocalities",
+"features": []
+};
+const newlocalities = {
+"type": "FeatureCollection",
+"name": "newlocalities",
+"features": []
+};
+
+
+const evaluate = (r) => {
+  reproject.parse(r);
+  let p = pointOnFeature(r).geometry.coordinates;
+  wofLookup.lookup({lat: p[0], long: p[1], layers: ['locality', 'localadmin']}).then(body => {
+    let wof = {};
+    body.forEach(e => {
+      if(e.Placetype == 'locality' && e.name.toLowerCase() == r.properties.NOM_COM.toLowerCase()) {
+        wof.locality = e;
+      } else if (e.Placetype == 'localadmin' && e.name.toLowerCase () == r.properties.NOM_COM.toLowerCase() ){
+        wof.localadmin = e;
+      }
+    });
+    if (wof.locality && wof.localadmin) {
+      r.properties['wof:locality_id'] = wof.locality.id;
+      r.properties['wof:localadmin_id'] = wof.localadmin.id;
+      localityAndLocaladmin.features.push(r)
+    } else if (!wof.locality && !wof.localadmin) {
+      newlocalities.features.push(r)
+    } else if (wof.locality) {
+      r.properties['wof:locality_id'] = wof.locality.id;
+      localities.features.push(r)
+    } else if (wof.localadmin) {
+      r.properties['wof:localadmin_id'] = wof.localadmin.id;
+      localadmin.features.push(r)
+    }
+  });
+}
+const shp = new ReadShapefile({
+  shp: shapefile,
+  dbf: dbf,
+  evaluate: {fn: evaluate, keep: ['NOM_COM']}
+})
+
+wofLookup.load().then(() => {
+  return shp.readAsync().then(r => {
+
+  });
+}).then(() => {
+  fs.writeFileSync('localities.geojson', JSON.stringify(localities));
+  fs.writeFileSync('localadmin.geojson', JSON.stringify(localadmin));
+  fs.writeFileSync('localityAndLocaladmin.geojson', JSON.stringify(localityAndLocaladmin));
+  fs.writeFileSync('newlocalities.geojson', JSON.stringify(newlocalities));
+});
